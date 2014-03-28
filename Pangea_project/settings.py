a8e0043d36b27864414d40f0487fbd767558242b
YashYash <yash.saxena1217@gmail.com>
2014-03-28 16:13:20 -0700

    more changes to requirements and settings

--- Pangea_project/settings.py
+++ Pangea_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Pangea_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pangea_project_databse',
         'USER': "root",
         'PASSWORD':"",
